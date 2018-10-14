c3156f6d6e0926761b4dcc34a6e53f460cf8838d
azmolsworld <md.azmolhossain@gmail.com>
2018-10-14 15:33:25 +0600

    Added database error function

--- classes/DB.php
+++ classes/DB.php
@@ -39,8 +39,16 @@ class DB{
 				}
 			}
 			if ($this->_query->execute()) {
-				echo "Hossain";
+				$this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
+				$this->_count = $this->_query->rowCount();
+			}else{
+				$this->_error = true;
 			}
 		}
+		return $this;
+	}
+
+	public function error(){
+		return $this->_error;
 	}
 }
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php 
 require_once 'core/init.php';
 
-DB::getInstance()->query("SELECT username FROM users WHERE username = ?", array('limon'));
+$use = DB::getInstance()->query("SELECT username FROM users WHERE username = ?", array('limon'));
+
+
+if ($use->error()) {
+	echo "No User";
+} else {
+	echo "Ok!";
+}
