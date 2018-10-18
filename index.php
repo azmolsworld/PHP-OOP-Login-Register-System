2f3a3d2bcfa00e9634c169f962002800236c1b81
azmolsworld <md.azmolhossain@gmail.com>
2018-10-18 21:04:35 +0600

    added update function

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
 require_once 'core/init.php';
 
-$use = DB::getInstance()->insert('users', array(
-	'username' 	=> 'bady',
-	'password' 	=> 'password',
-	'salt'		=> 'salt'
+$use = DB::getInstance()->update('users', 3, array(
+	'password' => 'new',
+	'name' => 'afifa'
 ));
+
